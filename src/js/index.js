957724d03d0a0ce521d8c1bd6a1a37ca4d1066c7
joao pedro <joaopedrolimar@gmail.com>
2023-09-22 14:18:56 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const personagens = document.querySelectorAll('.personagem');
+console.log(personagens);
+
+personagens.forEach( (personagem) => {
+
+    if(window.innerWidth < 450 ){
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+
+    personagem.addEventListener('mouseenter', () => {
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+
+        imagemPersonagemGrande.src = `./src/img/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+
+    })
+})
